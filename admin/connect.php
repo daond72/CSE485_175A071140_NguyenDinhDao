06b76f6f8bd1247b8523c8e3000673b5473661be
duytd72 <duytd72@wru.vn>
2020-11-13 02:23:33 +0700

    Update connect.php
    sửa connect

--- admin/connect.php
+++ admin/connect.php
@@ -1,15 +1,13 @@
-<?php 
-	$host = "localhost";
-	$user = "root";
-	$pass = "";
+<?php
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $db = 'cnweb';
 
-	try {
-		$con = new PDO("mysql:host=$host;dbname=dhtl", $user, $pass);
-		$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$con->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES 'utf8'");
-		$con->query('SET NAMES utf8');
-return $con;
-	} catch (PDOException $e) {
-		echo 'Connection Failed:' . $e->getMessage();
-	}
+    $conn = mysqli_connect($host, $user, $pass, $db);
+	mysqli_set_charset($conn, 'utf8');
+    if (!$conn) {
+        die ("Loi ket noi".mysqli_connect_error());
+        exit();
+    }
 ?>
