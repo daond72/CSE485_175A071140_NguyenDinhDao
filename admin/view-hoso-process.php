682e66d5cd042d513246ee92dcea7101e0d4286f
duytd72 <duytd72@wru.vn>
2020-11-13 10:00:15 +0700

    Update view-hoso-process.php

--- admin/view-hoso-process.php
+++ admin/view-hoso-process.php
@@ -1,8 +1,8 @@
 <?php
     require_once('connect.php');
     $mahoso = $_GET['mahoso'];
-    // $sql = "SELECT * FROM hoso INNER JOIN chitiethoso ON hoso.mahoso = chitiethoso.mahoso WHERE mahoso = $mahoso";
-    $sql = "SELECT * FROM hoso WHERE mahoso = $mahoso";
+    $sql = "SELECT * FROM hoso INNER JOIN chitiethoso ON hoso.mahoso = chitiethoso.mahoso WHERE mahoso = $mahoso";
+    // $sql = "SELECT * FROM hoso WHERE mahoso = $mahoso";
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_assoc($result);
     mysqli_close($conn);
