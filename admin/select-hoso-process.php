b29ec9898031ac051a1553905952834c434fc93b
duytd72 <duytd72@wru.vn>
2020-11-13 19:40:26 +0700

    Update select-hoso-process.php

--- admin/select-hoso-process.php
+++ admin/select-hoso-process.php
@@ -4,6 +4,8 @@ $sql = "SELECT * FROM hoso";
 $result = mysqli_query($conn, $sql);
 if ($result) {
 ?>
+<script src="js/script.js"></script>
+<h2>Quản lý hồ sơ</h2>
 <div class="container">
     <table class="table">
       <thead>
@@ -30,9 +32,9 @@ if ($result) {
           <td><?php echo $row['sdt'];?></td>
           <td><?php echo $row['diachi'];?></td>
           <td>
-            <a href="view.php?id=<?php echo $row['mahoso'];?>">view</a>&#160;
-            <a href="edit.php?id=<?php echo $row['mahoso'];?>">edit</a>&#160;
-            <a href="delete.php?id=<?php echo $row['mahoso'];?>" onclick="xacnhan()" id = "ok">delete</a>
+            <a href="view.php?mahoso=<?php echo $row['mahoso'];?>">view</a>&#160;
+            <a href="edithoso.php?mahoso=<?php echo $row['mahoso'];?>">edit</a>&#160;
+            <a href="#" onclick="xacnhan(<?php echo $row['mahoso'];?>)" id = "ok">delete</a>
           </td>
       </tr>
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
